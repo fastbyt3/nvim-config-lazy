b4ebc895d82a332667eab3422a05ceb9958d768e
test <test>
2025-11-03 09:26:00 +0530

    upd colorscheme

--- init.lua
+++ init.lua
@@ -4,6 +4,6 @@ require("custom-autocmd")
 require("plugins")
 require("lsp")
 
-vim.cmd.colorscheme("everforest")
+vim.cmd.colorscheme("catppuccin-mocha")
 -- set this for everforest to have the lsp signature highlight current arg in readable color
 vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { bg = "#3a3a3a", fg = "#ffffff" })
